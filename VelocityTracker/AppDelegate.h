c4f6c211241de9174ceac7a39db8a538e99666ad
dannyflax <drflax29@verizon.net>
2015-06-01 23:57:30 -0400

    Initial Commit
    Initial commit for a velocity tracking app

---
+++ VelocityTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VelocityTracker
+//
+//  Created by Danny Flax on 6/1/15.
+//  Copyright (c) 2015 Danny Flax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
